fd4ec9a108a3769c70603a3ebe8d180d30bf3dd8
Grant Wong <git@wong.nz>
2017-12-10 17:45:16 +1300

    Implement search! (by character, not Taishanese)

--- src/tsmc/urls.py
+++ src/tsmc/urls.py
@@ -6,4 +6,5 @@ app_name = 'tsmc'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.CharacterView.as_view(), name='character'),
+    url(r'^search/$', views.search, name='search'),
 ]
--- src/tsmc/views.py
+++ src/tsmc/views.py
@@ -1,4 +1,6 @@
 # from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
@@ -11,8 +13,47 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return ten arbitrarily-chosen characters."""
-        return Character.objects.all()[:25]
+        return Character.objects.all()[:10]
 
 class CharacterView(generic.DetailView):
     model = Character
     template_name = 'tsmc/character.html'
+
+def search(request):
+    page = request.GET.get('page', 1)
+
+    VALID_COLUMNS = ['char', 'initial', 'final', 'tone', 'openness', 'division', 'note']
+    # Filter out invalid query strings from user
+    columns = {column: request.GET.get(column, '') for column in VALID_COLUMNS}
+
+    char_q_objects = Q()
+    kwargs = {}
+
+    for (key, value) in columns.items():
+        if key == 'char':
+            for entry in list(value):
+                char_q_objects.add(Q(char=entry), Q.OR)
+        else:
+            kwargs[key + '__icontains'] = value
+
+    # Filter using the character field first
+    # 中文 will match 中 and 文
+    queryset1 = Character.objects.filter(char_q_objects)
+
+    # Filter using the other fields
+    queryset2 = queryset1.filter(**kwargs)
+
+    # Paginator
+    paginator = Paginator(queryset2, 20)
+
+    # Path used at the start of "Previous page"/"Next page" links
+    paginator_path = request.GET.copy()
+    if paginator_path.get('page', False):
+        del paginator_path['page']
+    paginator_path = '?' + paginator_path.urlencode() + '&'
+
+    # The characters the paginator spurts out
+    content = paginator.page(page)
+
+    context = {'character_list': content, 'length': len(queryset2), 'paginator_path': paginator_path}
+    return render(request, 'tsmc/index.html', context)
