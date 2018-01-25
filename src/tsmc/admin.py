fc6fab320c35ccccd2c3df4381bba44ffeb9ee71
Grant Wong <git@wong.nz>
2018-01-25 20:34:55 +1300

    Improve tsmc admin interface

--- src/tsmc/admin.py
+++ src/tsmc/admin.py
@@ -2,8 +2,20 @@ from django.contrib import admin
 
 from .models import Character, Taishanese
 
-class TaishaneseAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'char')
+class TaishaneseInline(admin.StackedInline):
+    model = Taishanese
+    extra = 1
 
-admin.site.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    inlines = [TaishaneseInline]
+    list_display = ('__str__', 'note')
+    save_on_top = True
+    search_fields = ('char',)
+
+class TaishaneseAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'char', 'note', 'source')
+    save_on_top = True
+    search_fields = ('initial','final','tone','note','source',)
+
+admin.site.register(Character, CharacterAdmin)
 admin.site.register(Taishanese, TaishaneseAdmin)
