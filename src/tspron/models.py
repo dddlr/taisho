beac5d4c98e4229e84e3f6f0c08e2c991364eabf
Grant Wong <git@wong.nz>
2018-05-19 19:56:16 +1200

    Make character limits longer in tspron

--- src/tspron/models.py
+++ src/tspron/models.py
@@ -4,8 +4,9 @@ from django.db import models
 
 class Word(models.Model):
     word = models.CharField("Chinese word or phrase", max_length=10)
-    pron = models.CharField("Pronunciation", max_length=20)
+    pron = models.CharField("Pronunciation", max_length=30)
     gloss = models.CharField("Gloss", max_length=50)
+    note = models.CharField("Note", max_length=100, blank=True)
 
     def __str__(self):
         return self.word
