5540cbb3ec5d40c20e3ebca9de1c0f7e0338dbd7
Grant Wong <git@wong.nz>
2018-05-19 19:56:33 +1200

    Make tspron admin interface slightly less shit

--- src/tspron/admin.py
+++ src/tspron/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Word
 
-admin.site.register(Word)
+class WordAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'pron', 'gloss', 'note')
+
+admin.site.register(Word, WordAdmin)
