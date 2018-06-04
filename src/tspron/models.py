8680da189ac0833250add45d318ea9a6a76c0f41
Grant Wong <git@wong.nz>
2018-06-04 13:14:29 +1200

    Increase gloss length to 100 characters

--- src/tspron/models.py
+++ src/tspron/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Word(models.Model):
     word = models.CharField("Chinese word or phrase", max_length=10)
     pron = models.CharField("Pronunciation", max_length=30)
-    gloss = models.CharField("Gloss", max_length=50)
+    gloss = models.CharField("Gloss", max_length=100)
     note = models.CharField("Note", max_length=100, blank=True)
 
     def __str__(self):
