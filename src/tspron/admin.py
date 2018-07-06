8575a60a240b920de64429dc46077e80a42ad37e
Grant Wong <git@wong.nz>
2018-07-06 13:22:43 +1200

    Add category to tspron

--- src/tspron/admin.py
+++ src/tspron/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import Word
+from .models import Category, Word
 
 class WordAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'pron', 'gloss', 'note')
+    list_display = ('__str__', 'pron', 'gloss', 'note', 'category')
 
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Word, WordAdmin)
--- src/tspron/models.py
+++ src/tspron/models.py
@@ -7,6 +7,21 @@ class Word(models.Model):
     pron = models.CharField("Pronunciation", max_length=30)
     gloss = models.CharField("Gloss", max_length=100)
     note = models.CharField("Note", max_length=100, blank=True)
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
 
     def __str__(self):
         return self.word
+
+class Category(models.Model):
+    name = models.CharField("Category name", max_length=30)
+
+    def __str__(self):
+        return self.name
+
+# class Sentence(models.Model):
+    # pass
