90e3bc1e3024dc368bbb96fe325878e7b28a629c
Grant Wong <git@wong.nz>
2022-11-27 10:29:00 +1300

    Set default_auto_field to make Django happy

--- src/tsmc/apps.py
+++ src/tsmc/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class TsmcConfig(AppConfig):
     name = 'tsmc'
+    default_auto_field = 'django.db.models.AutoField'
--- src/tspron/apps.py
+++ src/tspron/apps.py
@@ -2,3 +2,4 @@ from django.apps import AppConfig
 
 class TspronConfig(AppConfig):
     name = 'tspron'
+    default_auto_field = 'django.db.models.AutoField'
